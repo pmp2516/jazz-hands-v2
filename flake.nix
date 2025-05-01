203e39f43c5c2939c06bd8707e2a9a50d2185330
Patrick Philippy <pmp2516@rit.edu>
2025-05-01 13:59:19 -0400

    Removed Python dependencies

--- flake.nix
+++ flake.nix
@@ -1,59 +1,44 @@
 {
-  description = "Jazz Hands development environments, dependency closure, and application package.";
+  description = "Serve a static website using Docker.";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
-    uv2nix.url = "github:/adisbladis/uv2nix";
-    uv2nix.inputs.nixpkgs.follows = "nixpkgs";
-    uv2nix_hammer_overrides.url = "github:TyberiusPrime/uv2nix_hammer_overrides";
-    uv2nix_hammer_overrides.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    {
-      nixpkgs,
-      uv2nix,
-      uv2nix_hammer_overrides,
-      ...
-    }:
-    let
-      inherit (uv2nix.inputs) pyproject-nix;
-      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
 
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        siteDir = ./static;
+        sws = pkgs.static-web-server;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ sws ];
+          shellHook = ''
+            echo "static-web-server is available. You can start it with:"
+            echo "  static-web-server --root ${siteDir}"
+          '';
+        };
 
-      # Generate overlay
-      overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
-      pyprojectOverrides = pkgs.lib.composeExtensions (uv2nix_hammer_overrides.overrides pkgs) (
-        # use uv2nix_hammer_overrides.overrides_debug
-        #   to see which versions were matched to which overrides
-        #  use uv2nix_hammer_overrides.overrides_strict / overrides_strict_debug
-        #  to use only overrides exactly matching your python package versions
+        packages.default = pkgs.dockerTools.buildImage {
+          name = "static-web-server-site";
+          tag = "latest";
+          contents = [ sws siteDir ];
+          config = {
+            Cmd = [ "${sws}/bin/static-web-server" "--root" "${siteDir}" ];
+            ExposedPorts = { "80/tcp" = {}; };
+          };
+        };
 
-        _final: _prev: {
-          # place additional overlays here.
-          #a_pkg = prev.a_pkg.overrideAttrs (old: nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.someBuildTool] ++ (final.resolveBuildSystems { setuptools = [];});
-        }
-      );
-      interpreter = pkgs.python312;
-
-      # Construct package set
-      pythonSet' =
-        (pkgs.callPackage pyproject-nix.build.packages { python = interpreter; }).overrideScope
-          overlay;
-
-      # Override host packages with build fixups
-      pythonSet = pythonSet'.pythonPkgsHostHost.overrideScope pyprojectOverrides;
-      venv = pythonSet.mkVirtualEnv "jazz-hands-venv" workspace.deps.default;
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = [
-          pkgs.uv
-          venv
-        ];
-      };
-      devShells.x86_64-linux.uv = pkgs.mkShell { packages = [ nixpkgs.legacyPackages.x86_64-linux.uv ]; };
-    };
+        apps.default = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellApplication {
+            name = "serve-static";
+            text = ''
+              exec ${sws}/bin/static-web-server --root ${siteDir}
+            '';
+          };
+        };
+      });
 }
